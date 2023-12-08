c045aad97574d3cc59c2255bf4c17145d261c89e
jimothy <timothynewton1998@gmail.com>
2023-12-08 01:30:01 -0500

    solved

--- Day4/Day4.java
+++ Day4/Day4.java
@@ -21,8 +21,10 @@ public class Day4 {
         }
 
         int result1 = part1(cardsArray);
+        int result2 = part2(cardsArray);
 
-        System.out.println("Scratch-off sum: " + result1);
+        System.out.println("Scratch-off sum: " + result1 + "\n" + 
+        "Card Instances: " + result2);
 
         cardsScan.close();
 
@@ -62,16 +64,57 @@ public class Day4 {
 
             sum += points;
 
-
-
-
-
-
-
         }
         return sum;
     }
 
-    
-    
+
+    public static int part2(ArrayList<String> inputArray){
+        ArrayList<Integer> cardInstances = new ArrayList<>();
+        //initialize with 1's
+        for(String card : inputArray) {
+            cardInstances.add(1);
+        }
+
+        int cards = 0;
+
+        for(int i = 0; i < inputArray.size(); i++){
+            int currentInstances = cardInstances.get(i);
+            //lop off the card name
+            String cardNumbers = inputArray.get(i).split(":")[1];
+            String[] splitNumbers = cardNumbers.split("[|]");
+            //regex pattern matches one or more digits
+            Pattern singleWhiteSpace = Pattern.compile("\\d+");
+            Matcher matcher1 = singleWhiteSpace.matcher(splitNumbers[0]);
+            Matcher matcher2 = singleWhiteSpace.matcher(splitNumbers[1]);
+
+            int timesWon = 0;
+
+            while(matcher1.find()){
+                while(matcher2.find()){
+                    // System.out.println(matcher1.group() + " compared to " + matcher2.group()); 
+                    if (matcher1.group().equals(matcher2.group())){
+                        timesWon += 1;
+                    }
+                }
+                matcher2.reset();
+            }
+
+            for(int ii = i + 1; ii < i + timesWon + 1; ii++){
+
+                if (ii == inputArray.size() - 1){
+                    cardInstances.set(ii, cardInstances.get(ii) + currentInstances);
+                    break;
+                } else {
+                    cardInstances.set(ii, cardInstances.get(ii) + currentInstances);
+                }                    
+            }
+        }
+
+        for(int instances : cardInstances){
+            cards += instances;
+        }
+
+        return cards;
+    }    
 }
